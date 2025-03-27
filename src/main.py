02be5fd99d04822c494514c132c17df0452f34b9
Luis Eduardo Imbernón <imbernon@gmail.com>
2025-03-27 18:29:15 +0100

    Added a better stt model. This model works locally and does not need any online service.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from service import queue_service
 from threading import Thread
-import qi
 
 if __name__ == "__main__":
 
@@ -8,7 +7,3 @@ if __name__ == "__main__":
     t = Thread(target=queue_service.start_consuming)
     t.start()
 
-    #session = qi.Session()
-    #session.connect("tcp://192.168.0.101:9559")
-    #tts = session.service('ALTextToSpeech')
-    #tts.say("Hola!")
--- src/service/speech_service.py
+++ src/service/speech_service.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+from whisper_mic import WhisperMic
 import beepy as beep
 
 class SpeechService:
@@ -9,22 +10,40 @@ class SpeechService:
 
     def listen(self):
         dest = None
-        with sr.Microphone() as source:
-            #self.r.adjust_for_ambient_noise(source)
+
+        try:
+            mic = WhisperMic()
+            if not mic.mic_active:
+                mic.toggle_microphone()
+
             print("Please, say something")
             beep.beep('coin')
 
-            audio = self.r.listen(source, timeout=10)
+            dest = mic.listen()
+
             print("Recognizing Now... ")
             beep.beep('ping')
 
-            try:
-                dest = self.r.recognize_google(audio, language=self.language)
-                #dest = self.r.recognize_openai(audio, language=self.language)
-                #dest = self.r.recognize_whisper(audio, language=self.language)
-                dest = dest.replace(',', '').replace('"', '')
-                print("You have said: " + dest)
-            except Exception as e:
-                print("Error :" + str(e))
+            dest = dest.replace(',', '').replace('"', '')
+            print("You have said: " + dest)
+
+        except Exception as e:
+            print("Error :" + str(e))
+
+        #with sr.Microphone() as source:
+            #self.r.adjust_for_ambient_noise(source)
+            #print("Please, say something")
+            #beep.beep('coin')
+
+            #audio = self.r.listen(source, timeout=10)
+            #print("Recognizing Now... ")
+            #beep.beep('ping')
+
+            #try:
+               # dest = self.r.recognize_google(audio, language=self.language)
+                #dest = dest.replace(',', '').replace('"', '')
+                #print("You have said: " + dest)
+            #except Exception as e:
+                #print("Error :" + str(e))
 
         return dest
