7452bb899ad9367d5734cddd67620210d9109aae
Luis Eduardo Imbernón <imbernon@gmail.com>
2025-11-30 18:53:16 +0100

    Refactor speech service to integrate Whisper model for improved transcription accuracy and enhance listening functionality

--- src/service/nao_service.py
+++ src/service/nao_service.py
@@ -183,7 +183,7 @@ class NAOService:
 
         # Resets the posture
         posture = self.session.service('ALRobotPosture')
-        posture.goToPosture('Rest', 0.5)
+        posture.goToPosture('StandInit', 0.5)
 
         # Once the robot resets, we store the last execution timestamp
         self.last_bmle_execution_time = datetime.datetime.now()
